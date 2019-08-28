e0bb853aa31b19251154c34313bcb5ba90defb3b
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-08-28 14:42:39 -0700

    add feature toggles

--- src/util/kafkaTracking.js
+++ src/util/kafkaTracking.js
@@ -10,6 +10,7 @@
  * /kafkaTracking.js
  */
 const logger = require('@salesforce/refocus-logging-client');
+const featureToggles = require('feature-toggles');
 
 const { pubSubAggregationTopic } = require('../../conf/config');
 
@@ -22,40 +23,45 @@ const parseDate = (date) => typeof date === 'number' ?
   new Date(date * 1000).toISOString() : date;
 
 const trackEmit = (sampleName, updatedAt, numClientsEmittedTo) => {
-  if (typeof sampleName !== 'string' || !(typeof updatedAt === 'string' ||
-    typeof updatedAt === 'number') || typeof numClientsEmittedTo !== 'number') {
-      logger.error(`Received invalid args in trackEmit: ${sampleName} ${updatedAt} ${numClientsEmittedTo}`);
-      return;
-  }
+  if (featureToggles.isFeatureEnabled('enableKafkaPubSubAggregation')) {
+    if (typeof sampleName !== 'string' || !(typeof updatedAt === 'string' ||
+      typeof updatedAt === 'number') || typeof numClientsEmittedTo !== 'number') {
+        logger.error(`Received invalid args in trackEmit: ${sampleName} ${updatedAt} ${numClientsEmittedTo}`);
+        return;
+    }
 
-  updatedAt = parseDate(updatedAt);
-  logger.track({
-    type: MESSAGE_TYPES.EMITTED,
-    emittedAt: Date.now(),
-    numClientsEmittedTo
-  },
-  'info', pubSubAggregationTopic, {
-    sampleName,
-    updatedAt,
-  });
+    updatedAt = parseDate(updatedAt);
+    logger.track({
+      type: MESSAGE_TYPES.EMITTED,
+      emittedAt: Date.now(),
+      numClientsEmittedTo
+    },
+    'info', pubSubAggregationTopic, {
+      sampleName,
+      updatedAt,
+    });
+  }
 }
 
 const trackClient = (sampleName, updatedAt, timeReceived) => {
-  if (typeof sampleName !== 'string' || !(typeof updatedAt === 'string' ||
-    typeof updatedAt === 'number') || typeof timeReceived !== 'number') {
-      logger.error(`Received invalid args in trackClient: ${sampleName} ${updatedAt} ${timeReceived}`);
-      return;
-  }
+  if (featureToggles.isFeatureEnabled('enableKafkaPubSubAggregation')) {
+    if (typeof sampleName !== 'string' || !(typeof updatedAt === 'string' ||
+      typeof updatedAt === 'number') || typeof timeReceived !== 'number') {
+        logger.error(`Received invalid args in trackClient: ${sampleName} ${updatedAt} ${timeReceived}`);
+        return;
+    }
 
-  updatedAt = parseDate(updatedAt);
-  logger.track({
-    type: MESSAGE_TYPES.ACKNOWLEDGED,
-    timeReceived
-  },
-  'info', pubSubAggregationTopic, {
-    sampleName,
-    updatedAt,
-  });
+    updatedAt = parseDate(updatedAt);
+    logger.track({
+      type: MESSAGE_TYPES.ACKNOWLEDGED,
+      timeReceived
+    },
+    'info', pubSubAggregationTopic, {
+      sampleName,
+      updatedAt,
+    });
+  }
+    
 }
 
 module.exports = {
